15dfca25de0a657e7566f6f836cadd93fa77ff13
LeVunguyen <levunguyendn@gmail.com>
2017-12-13 16:08:30 +0700

    init project

---
+++ src/main/java/com/football4me/footballautomation/App.java
@@ -0,0 +1,13 @@
+package com.football4me.footballautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
